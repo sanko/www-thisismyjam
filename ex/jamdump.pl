82d5ce971f16a1d3ac75d73d6e7bf0fdf054ac5f
Sanko Robinson <sanko@cpan.org>
2013-05-10 18:14:50 -0400

    Very simple example script to archive a user's jams

---
+++ ex/jamdump.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use WWW::ThisIsMyJam;
+#
+my $timj = WWW::ThisIsMyJam->new();
+my @jams;
+for (my $page = 1;; ++$page) {
+    my $r = $timj->jams({person => 'jamoftheday', page => $page});
+    push @jams, $r->{jams};
+    last unless $r->{list}{hasMore};
+}
+
+# Do something with @jams.
